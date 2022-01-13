748215638960ed155ee92123f4dc8760c7466936
abearab <abardesigner@gmail.com>
2022-01-13 13:23:55 -0800

    minor fix

--- parse_metrics.sh
+++ parse_metrics.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ID=$1
 
-input=${ID}_alignment_metrics.txt
+
+input=${ID}alignment_metrics.txt
 while read line
 do
 	if [[ $line == PAIR* ]];then
@@ -10,7 +11,9 @@ do
 done < $input
 
 re='^[0-9]+([.][0-9]+)?$'
-input=${ID}_insert_metrics.txt
+
+
+input=${ID}insert_metrics.txt
 while read line
 do
 	MEAN_INSERT_SIZE=$(echo $line | cut -d' ' -f5)
@@ -19,11 +22,11 @@ do
 	fi
 done < $input
 
-snps_1=$(grep -v '^#' ${ID}_raw_snps.vcf | wc -l)
-snps_2=$(grep 'PASS' ${ID}_filtered_snps.vcf | wc -l)
-snps_3=$(grep -v '^#' ${ID}_raw_snps_recal.vcf | wc -l)
-snps_4=$(grep 'PASS' ${ID}_filtered_snps_final.vcf | wc -l)
-avg_coverage=$(awk '{sum+=$3} END { print sum/NR}' ${ID}_depth_out.txt)
 
-echo "$ID,$ALIGNMENT_METRICS,$MEAN_INSERT_SIZE,$snps_1,$snps_2,$snps_3,$snps_4,$avg_coverage" >> ../report.csv
+snps_1=$(grep -v '^#' ${ID}raw_snps.vcf | wc -l)
+snps_2=$(grep 'PASS' ${ID}filtered_snps.vcf | wc -l)
+snps_3=$(grep -v '^#' ${ID}raw_snps_recal.vcf | wc -l)
+snps_4=$(grep 'PASS' ${ID}filtered_snps_final.vcf | wc -l)
+avg_coverage=$(awk '{sum+=$3} END { print sum/NR}' ${ID}depth_out.txt)
 
+echo "$ID,$ALIGNMENT_METRICS,$MEAN_INSERT_SIZE,$snps_1,$snps_2,$snps_3,$snps_4,$avg_coverage" > ${ID}.report.csv
