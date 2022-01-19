0f3d534fb538945989d89e972d50690aa801e0e0
Abolfazl (Abe) <53412130+abearab@users.noreply.github.com>
2022-01-18 17:07:06 -0800

    reformat file names

--- gatk-part1.sh
+++ gatk-part1.sh
@@ -8,15 +8,42 @@ mkdir ${sampleid}
 # all outputs are made in the same folder 
 
 #input is bam files, output is bam file without duplicate reads
-gatk MarkDuplicates CREATE_INDEX=true INPUT=${bam_file} OUTPUT=${sampleid}/${sampleid}.bam METRICS_FILE=${sampleid}/${sampleid}.dups.mtx VALIDATION_STRINGENCY=STRICT MAX_FILE_HANDLES=1000
+gatk MarkDuplicates CREATE_INDEX=true \
+  INPUT=${bam_file} \
+  OUTPUT=${sampleid}/${sampleid}.dups.bam \
+  METRICS_FILE=${sampleid}/${sampleid}.dups.mtx \
+  VALIDATION_STRINGENCY=STRICT \
+  MAX_FILE_HANDLES=1000
 
 #input is bam file from step above, output is bam file with correct read tags
-gatk AddOrReplaceReadGroups I=${sampleid}/${sampleid}.bam O=${sampleid}/${sampleid}out.bam RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=20
+gatk AddOrReplaceReadGroups \
+  I=${sampleid}/${sampleid}.dups.bam \
+  O=${sampleid}/${sampleid}.ctags.bam \
+  RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=20
 
 #input is reference and updated bam file, output is alignment file
-gatk CollectAlignmentSummaryMetrics  R=$genome_fa I=${sampleid}/${sampleid}out.bam O=${sampleid}/${sampleid}alignment_metrics.txt
+gatk CollectAlignmentSummaryMetrics  \
+  R=$genome_fa \
+  I=${sampleid}/${sampleid}.ctags.bam \
+  O=${sampleid}/${sampleid}.alignment_metrics.txt
 
 #input is updated bam file, output is metrics file and histogram plots
-gatk CollectInsertSizeMetrics I=${sampleid}/${sampleid}out.bam  O=${sampleid}/${sampleid}insert_metrics.txt H=${sampleid}/${sampleid}insert_size_histogram.pdf
+gatk CollectInsertSizeMetrics \
+  I=${sampleid}/${sampleid}.ctags.bam  \
+  O=${sampleid}/${sampleid}.insert_metrics.txt \
+  H=${sampleid}/${sampleid}.insert_size_histogram.pdf
 
-samtools depth -a ${sampleid}/${sampleid}out.bam  > ${sampleid}/${sampleid}depth_out.txt
+#input is reference and updated bam and first data table, output is bam file of recalibrated reads
+gatk ApplyBQSR \
+  -R $genome_fa \
+  -I ${sampleid}/${sampleid}.ctags.bam \
+  -bqsr ${sampleid}/${sampleid}.recal_data.table \
+  -O ${sampleid}/${sampleid}.recal_reads.bam 
+
+#input is reference and updated bam and first data table, output is bam file of recalibrated reads
+gatk ApplyBQSR -R $genome_fa \
+  -I ${sampleid}/${sampleid}.ctags.bam \
+  -bqsr ${sampleid}/${sampleid}.recal_data.table \
+  -O ${sampleid}/${sampleid}.recal_reads.bam 
+
+samtools depth -a ${sampleid}/${sampleid}.ctags.bam  > ${sampleid}/${sampleid}.depth_out.txt
