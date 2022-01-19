1084bba3cf7d59ed452b4b524e4aacbe55a8f1e3
Abolfazl (Abe) <53412130+abearab@users.noreply.github.com>
2022-01-18 17:31:19 -0800

    Update gatk-part3.sh

--- gatk-part3.sh
+++ gatk-part3.sh
@@ -5,8 +5,8 @@ echo $sampleID;
 java -jar \
 	${WES}tools/snpEff/snpEff.jar \
 	-v GRCh38.99 \
-	${sampleID}/${sampleID}filtered_snps_final.vcf &> ${sampleID}/${sampleID}filtered_snps_final.ann.vcf;
+	${sampleID}/${sampleID}.filtered_snps_final.vcf &> ${sampleID}/${sampleID}.filtered_snps_final.ann.vcf;
 wait;
 
-mv -v snpEff_genes.txt ${sampleID}/${sampleID}snpEff_genes.txt
-mv -v snpEff_summary.html ${sampleID}/${sampleID}snpEff_summary.html
+mv -v snpEff_genes.txt ${sampleID}/${sampleID}.snpEff_genes.txt
+mv -v snpEff_summary.html ${sampleID}/${sampleID}.snpEff_summary.html
