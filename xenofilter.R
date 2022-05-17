36c3588fa22a6202ccc7c7cf1801158d88bf1603
Sonakshi Bhalla <32502327+sonakshibhalla@users.noreply.github.com>
2022-05-17 14:41:19 -0700

    Update xenofilter.R

--- xenofilter.R
+++ xenofilter.R
@@ -1,1 +1,19 @@
+library("XenofilteR")
+
+bp.param <- SnowParam(workers = 10, type = "SOCK")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+samplelist <- args[1]
+inputFolder <- args[2]
+
+sample.list <- read.csv(samplelist, header = FALSE)
+
+output.names <- gsub('.human.srt.bam', '.processed.bam', sample.list$'V1')
+
+output.names <- gsub(inputFolder, '', output.names)
+
+XenofilteR(sample.list, destination.folder = "./", bp.param = bp.param, output.names,
+           MM_threshold = 12, Unmapped_penalty = 4
+
 
