13bf316269117ee410378a72e0b254392b0e5998
Abolfazl (Abe) <53412130+abearab@users.noreply.github.com>
2022-07-11 15:17:02 -0700

    Update xenofilter.R

--- xenofilter.R
+++ xenofilter.R
@@ -2,28 +2,34 @@ library("XenofilteR")
 
 args <- commandArgs(trailingOnly = TRUE)
 
-samplelist <- args[1]
-inputFolder <- args[2]
-JOBS <- args[3]
-align_mouse = args[4]
-align_human = args[5]
+align_mouse = args[1]
+pattern_mouse = args[2] # *.mm10.srt
 
-# make a dataframe/table with these two lists
-list.files(pattern='*.mm10.srt.bam$',path=align_mouse,full.names = TRUE, recursive=T)
+align_human = args[3]
+pattern_human = args[4] # *.hg38.srt
 
-list.files(pattern='*.hg38.srt.bam$',path=align_human,full.names = TRUE, recursive=T)
-
-# figure out how to create a table with two lists
+xenofilterDir = args[5]
+JOBS <- args[6]
 
 bp.param <- SnowParam(workers = JOBS, type = "SOCK")
 
-sample.list <- read.csv(samplelist, header = FALSE)
+## make a dataframe/table with these two lists
+# samplelist <- args[1]
+# sample.list <- read.csv(samplelist, header = FALSE)
 
-output.names <- gsub('.human.srt.bam', '.processed.bam', sample.list$'V1')
+sample.list <- data.frame(
+	human=list.files(pattern=paste0(pattern_human,'.bam$'),path=align_human,full.names = TRUE, recursive=T),
+	mouse=list.files(pattern=paste0(pattern_mouse,'.bam$'),path=align_mouse,full.names = TRUE, recursive=T)
+)
 
-output.names <- gsub(inputFolder, '', output.names)
+output.names <- gsub(paste0(pattern_human,'.bam$'), '.xenofilter.bam', sample.list$human)
+output.names <- gsub(align_human, '/', output.names)
 
-XenofilteR(sample.list, destination.folder = "./", bp.param = bp.param, output.names,
-           MM_threshold = 12, Unmapped_penalty = 4
-
-
+XenofilteR(
+           sample.list, 
+           destination.folder = xenofilterDir, 
+           bp.param = bp.param, 
+           output.names,
+           MM_threshold = 12, 
+           Unmapped_penalty = 4
+)
