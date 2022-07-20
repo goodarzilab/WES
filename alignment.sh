a39d8f95bfb875b95a43bca7b72b8b52b813e61f
abearab <abardesigner@gmail.com>
2022-07-19 20:47:25 -0700

    update script

--- alignment.sh
+++ alignment.sh
@@ -8,12 +8,12 @@ mkdir $bamDir
 
 for fq1 in $fastqDir/*_1.fastq.gz; do
 	base=`basename $fq1`
-	fq2=${fq1/1.fastq.gz/_2.fastq.gz}; # the R1 pattern might be different!
+	fq2=${fq1/_1.fastq.gz/_2.fastq.gz}; # the R1 pattern might be different!
 	out=${base/_1.fastq.gz/};
 	echo $out;
-	bwa mem -t $JOBS ${genomeDir}/${genomeRef}.fa $fq1 $fq2 | samtools view -bS - &> bam/${out}.${genomeRef}.bam;
-	#cd $bamDir
-	#samtools sort -@ $JOBS -m 4G -o ${out}.${genomeRef}.srt.bam ${out}.${genomeRef}.bam
-	#samtools index ${out}.${genomeRef}.srt.bam;
-	#cd ../
+	bwa mem -t $JOBS ${genomeDir}/${genomeRef}.fa $fq1 $fq2 | samtools view -bS - &> ${bamDir}/${out}.${genomeRef}.bam;
+	cd $bamDir
+	samtools sort -@ $JOBS -m 4G -o ${out}.${genomeRef}.srt.bam ${out}.${genomeRef}.bam
+	samtools index ${out}.${genomeRef}.srt.bam;
+	cd ../
 done
